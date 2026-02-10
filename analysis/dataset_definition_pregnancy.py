e602bdd68fb87f0e68137011465527833dc7228e
HelenCEBM <helen.curtis@thedatalab.org>
2026-02-10 17:04:53 +0000

    calculate gestation

--- analysis/dataset_definition_pregnancy.py
+++ analysis/dataset_definition_pregnancy.py
@@ -1,4 +1,4 @@
-from ehrql import create_dataset
+from ehrql import create_dataset, weeks, show
 from ehrql.tables.tpp import patients, practice_registrations, clinical_events
 import codelists
 dataset = create_dataset()
@@ -10,23 +10,30 @@ index_date = "2025-11-30"
 #registration_start = practice_registrations.for_patient_on(start_date)
 #registration_end = practice_registrations.for_patient_on(index_date)
 
-# create a subset of events
-selected_events = clinical_events.where(
-    clinical_events.date.is_on_or_between(start_date, index_date)
-)
+dataset.pregnancy_end = clinical_events.where(
+    clinical_events.snomedct_code.is_in(codelists.end_pregnancy_codelist)).sort_by(clinical_events.date).last_for_patient().date
 
-pregnancy_end = selected_events.where(
-    selected_events.snomedct_code.is_in(codelists.end_pregnancy_codelist)
-)
+#dataset.pregnancy_end = clinical_events.where(
+#    clinical_events.snomedct_code.is_in(codelists.end_pregnancy_codelist)).sort_by(clinical_events.date).last_for_patient().date
+
 
 # estimated date of delivery
 # we can use this to estimate the start of pregnancy 9 months earlier
-pregnancy_edd = selected_events.where(
-    selected_events.snomedct_code.is_in(codelists.edd_codes)
-)
+dataset.pregnancy_edd = clinical_events.where(clinical_events.snomedct_code.is_in(codelists.edd_codes)).sort_by(clinical_events.date).last_for_patient().date
+
+# calculate gestation at end of pregnancy
+pregnancy_how_early = (dataset.pregnancy_edd - dataset.pregnancy_end).weeks
+dataset.pregnancy_length_from_edd = 40 - pregnancy_how_early
+
+
+# for start of pregnancy, assume pregnancy status known from 8 weeks
+dataset.pregnancy_known_date = dataset.pregnancy_edd - weeks(32)
+
 
 dataset.sex = patients.sex
 dataset.age = patients.age_on(index_date)
 dataset.define_population(
     (patients.sex == "female") & (dataset.age <=50) & (dataset.age >=11)
 )
+
+show(dataset)
